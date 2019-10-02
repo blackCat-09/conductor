c0bf2f9ce12293651c12dfec2e0a1ba9f365afb3
blackCat-09 <wodrjing@163.com>
2019-10-02 23:11:30 +0800

    test1

--- server/src/main/java/com/netflix/conductor/bootstrap/Main.java
+++ server/src/main/java/com/netflix/conductor/bootstrap/Main.java
@@ -109,7 +109,7 @@ public class Main {
         });
 
     }
-    // 加载配置文件1
+    // 加载配置文件2
     private static void loadConfigFile(String propertyFile) throws IOException {
         if (propertyFile == null) return;
         System.out.println("Using config file: " + propertyFile);
